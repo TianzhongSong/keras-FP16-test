087b7b8c6617f49801d293047c814e2f1f2c3abc
tianz <ssstz@outlook.com>
2018-06-30 11:08:49 +0800

    update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import argparse
-from models import vgg16, inception_v3, resnet_50, mobilenet, densenet_121, xception, inception_resnet_v2
+from models import vgg16, inception_v3, resnet_50, mobilenet, densenet_121, xception, inception_resnet_v2, squeezenet
 from pkl_reader import DataGenerator
 from keras import backend as K
 import numpy as np
@@ -18,10 +18,11 @@ def top5_acc(pred, k=5):
 
 if __name__ == "__main__":
     parse = argparse.ArgumentParser(description='command for testing keras model with fp16 and fp32')
-    parse.add_argument('--model', type=str, default='xception', help='support vgg16, resnet50, densenet121, inceptionv3, inception_resnet_v2, xception, mobilenet')
-    parse.add_argument('--weights', type=str, default='./weights/xception_weights_tf_dim_ordering_tf_kernels.h5')
-    parse.add_argument('--dtype', type=str, default='float32')
-    parse.add_argument('--alpha', type=float, default=0.25, help='alpha for mobilenet')
+    parse.add_argument('--model', type=str, default='squeezenet', help='support vgg16, resnet50, densenet121, \
+     inceptionv3, inception_resnet, xception, mobilenet, squeezenet')
+    parse.add_argument('--weights', type=str, default='./weights/squeezenet_weights_tf_dim_ordering_tf_kernels.h5')
+    parse.add_argument('--dtype', type=str, default='float16')
+    parse.add_argument('--alpha', type=float, default=1., help='alpha for mobilenet')
     args = parse.parse_args()
 
     K.set_floatx(args.dtype)
@@ -39,6 +40,8 @@ if __name__ == "__main__":
         model = resnet_50.ResNet50(input_shape=(224, 224, 3))
     elif args.model == 'densenet':
         model = densenet_121.DenseNet(reduction=0.5, classes=1000)
+    elif args.model == 'squeezenet':
+        model = squeezenet.SqueezeNet(input_shape=(227, 227, 3), classes=1000)
     elif args.model == 'mobilenet':
         model = mobilenet.MobileNet(input_shape=(224, 224, 3), alpha=args.alpha)
     else:
@@ -46,7 +49,7 @@ if __name__ == "__main__":
     # model.summary()
     model.load_weights(args.weights)
 
-    print('Evaluation {0} with {1} data type'.format(args.model, args.dtype))
+    print('Evaluating {0} with {1} data type'.format(args.model, args.dtype))
 
     dg = DataGenerator('./imagenet_val/val224_compressed.pkl', model=args.model, dtype=args.dtype)
     acc = 0
