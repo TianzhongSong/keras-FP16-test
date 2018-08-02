4681bc9539c0d325823b595909ecd48bc03e2cb0
Tianz <ssstz@outlook.com>
2018-08-02 17:15:56 +0800

    Update segdata_generator.py

--- utils/segdata_generator.py
+++ utils/segdata_generator.py
@@ -1,75 +1,47 @@
 import numpy as np
 import cv2
-import itertools
+import random
 import os
 import sys
 
 sys.path.append('../')
 
 
-def getImageArr(path, width, height, imgNorm="sub_mean"):
-    try:
-        img = cv2.imread(path, 1)
-        if imgNorm == "sub_and_divide":
-            img = np.float32(img) / 127.5 -1
-        elif imgNorm == "sub_mean":
-            img = cv2.resize(img, (width, height))
-            img = img.astype(np.float32)
-            img[:, :, 0] -= 103.939
-            img[:, :, 1] -= 116.779
-            img[:, :, 2] -= 123.68
-        elif imgNorm == "divide":
-            img = cv2.resize(img, (width, height))
-            img = img.astype(np.float32)
-            img = img / 255.0
-        return img
-    except Exception as e:
-        print(path, e)
-        img = np.zeros((height, width, 3))
-        return img
-
-
-def getSegmentationArr(path, nClasses, width, height):
-    seg_labels = np.zeros((height, width, nClasses))
-    try:
-        img = cv2.imread(path, 1)
-        img = cv2.resize(img, (width, height))
-        img = img[:, :, 0]
-
+def get_batch(items, root_path, nClasses, height, width, train=True):
+    x = []
+    y = []
+    for item in items:
+        image_path = root_path + item.split(' ')[0]
+        label_path = root_path + item.split(' ')[-1].strip()
+        img = cv2.imread(image_path, 1)
+        label_img = cv2.imread(label_path, 1)
+        # random flip images during training
+        if train:
+            is_flip = random.randint(0, 1)
+            if is_flip == 1:
+                img = cv2.flip(img, 1)
+                label_img = cv2.flip(label_img, 1)
+        label_img = label_img[:, :, 0]
+        seg_labels = np.zeros((height, width, nClasses))
         for c in range(nClasses):
-            seg_labels[:, :, c] = (img == c).astype(int)
+            seg_labels[:, :, c] = (label_img == c).astype(int)
+        img = np.float32(img) / 127.5 - 1
+        seg_labels = np.reshape(seg_labels, (width * height, nClasses))
+        x.append(img)
+        y.append(seg_labels)
+    return x, y
 
-    except Exception as e:
-        print(e)
 
-    seg_labels = np.reshape(seg_labels, (width * height, nClasses))
-    return seg_labels
-
-
-def imageSegmentationGenerator(images_path, segs_path, batch_size, n_classes, input_height, input_width, output_height,
-                               output_width):
-    images = os.listdir(images_path)
-    segmentations = os.listdir(segs_path)
-    images.sort()
-    segmentations.sort()
-    for i in range(len(images)):
-        images[i] = images_path + images[i]
-        segmentations[i] = segs_path + segmentations[i]
-
-    assert len(images) == len(segmentations)
-    for im, seg in zip(images, segmentations):
-        im = im.replace('\\', '/')
-        seg = seg.replace('\\', '/')
-        assert (im.split('/')[-1].split(".")[0] == seg.split('/')[-1].split(".")[0])
-
-    zipped = itertools.cycle(zip(images, segmentations))
+def generator(root_path, path_file, batch_size, n_classes, input_height, input_width):
+    items = os.listdir(path_file)
 
     while True:
-        X = []
-        Y = []
-        for _ in range(batch_size):
-            im, seg = next(zipped)
-            X.append(getImageArr(im, input_width, input_height))
-            Y.append(getSegmentationArr(seg, n_classes, output_width, output_height))
-
-        yield np.array(X), np.array(Y)
+        shuffled_items = []
+        index = [n for n in range(len(items))]
+        random.shuffle(index)
+        for i in range(len(items)):
+            shuffled_items.append(items[index[i]])
+        for j in range(len(items) // batch_size):
+            x, y = get_batch(shuffled_items[j * batch_size:(j + 1) * batch_size],
+                             root_path, n_classes, input_height, input_width)
+            yield np.array(x), np.array(y)
